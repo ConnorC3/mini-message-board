71ef73d3f1a2a5ecfe04b8f6c392b23637b62608
Connor C <164671442+ConnorC3@users.noreply.github.com>
2025-09-05 19:52:18 -0400

    Add basic functionality

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello Wolrd");
-});
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+const indexRouter = require("./routes/indexRouter");
+app.use("/", indexRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
---
+++ routes/indexRouter.js
@@ -0,0 +1,32 @@
+const { Router, text } = require("express");
+
+const indexRouter = Router();
+
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+];
+
+indexRouter.get("/", (req, res) => {
+    res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+indexRouter.get("/new", (req, res) => {
+    res.render("form", { title: "New Message "});
+});
+
+indexRouter.post("/new", (req, res) => {
+    const { user, text } = req.body;
+    messages.push({ text, user, added: new Date() });
+    res.redirect("/");
+});
+
+module.exports = indexRouter;
